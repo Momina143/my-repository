ab6c7bd5c8d41a6426e992c07f46acc871bacdca
momina <momina.t@gmail.com>
2025-01-30 17:08:24 -0700

    This is my second flask project

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,25 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello World"
 
+@app.route("/second")
+def second():
+    return "This is the second page"
 
+@app.route("/third/thirdsubpage")
+def third():
+    return "this is a subpage of the 3rd page, /third/thirdSub"
 
+@app.route("/fourth/<string:id>")
+def fourth(id):
+    output = f"Your ID is {id}"
+    return output
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    app.run(debug=True, port=5000)
+    # app.run(host= '0.0.0.0', port=8081) 
