23d78a962a6ac3fa6be89bd746ed0b550d205fa5
momina <momina.t@gmail.com>
2025-02-23 01:30:02 -0700

    final changes for roman numerals

--- 001-roman-numerals-converter/userdata.sh
+++ 001-roman-numerals-converter/userdata.sh
@@ -8,7 +8,23 @@ yum install python3 -y
 yum install pip -y
 pip install flask
 
-# install the code
+### install the code ###
+
+# change the user directory
 cd /home/ec2-user
 
-wget 
+# get the app.py file into current directory
+FOLDER="https://raw.githubusercontent.com/Momina143/my-repository/refs/heads/main/001-roman-numerals-converter"
+wget ${FOLDER}/app.py
+
+# get the templates into the templates/ directory
+mkdir templates
+cd templates
+wget ${FOLDER}/templates/index.html
+wget ${FOLDER}/templates/result.html
+
+# run the python application
+cd ..
+python3 app.py
+
+
